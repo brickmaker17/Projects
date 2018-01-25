12d8eacf6310c00cc3cef998d15cb1f0ff3e7a05
Luke Barrett <gotluke@gmail.com>
2018-01-24 17:07:19 -0700

    updated personal react site

--- personal-react-redux-site/src/App/Header/index.js
+++ personal-react-redux-site/src/App/Header/index.js
@@ -5,18 +5,41 @@ import { getCoins } from "../../redux/coins";
 import "./index.css";
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchTerm: ""
+        }
+    }
     componentDidMount() {
         this.props.getCoins()
     }
-    render() {
-        const coins = this.props.coins.map((coin, index) =>
-            <Link to={`/coins/${coin.id}`} key={index}><span className={`s-${coin.id}`} key={index}></span>{coin.name}</Link>
-        )
+    handleSearch = (e) => {
+        let { value } = e.target;
+        this.setState({
+            searchTerm: value
+        })
+    }
 
+    render() {
+        const coinList = this.props.coins.filter((coin) => {
+            return coin.name.toLowerCase().includes(this.state.searchTerm);
+        }).map((coin, index) =>
+            <li key={index}><Link to={`/coins/${coin.id}`} key={index}><span className={`s-${coin.id}`} key={index}></span>{coin.name}</Link></li>
+            )
+
+
+        // const coinFilter = this.props.coins.filter((coin, index) => {
+        // return coin.name.indexof(`${coin}`) >= 0
+        // })
         return (
-            <div className="nav">
-                
-                    {coins}
+            <div>
+                <input value={this.state.searchTerm} type="text" placeholder="Search" onChange={this.handleSearch} />
+                <div className="view">
+                    <ul className="nav">
+                        {coinList}
+                    </ul>
+                </div>
             </div>
         )
     }
--- personal-react-redux-site/src/App/index.js
+++ personal-react-redux-site/src/App/index.js
@@ -6,12 +6,16 @@ import './index.css';
 
 function App(props) {
     return (
-        <div className= "content">
-            <Header />
-            <Switch>
-                <Route path="/coins/:id" component={Coin} />
-            </Switch>
+        <div>
+            <h1 className = "head">Crypto Coin Price Lookup</h1>
+            <div className="content">
 
+                <Header />
+                <Switch>
+                    <Route path="/coins/:id" component={Coin} />
+                </Switch>
+            </div>
+            <footer className="foot">©Luke Barrett <a href="https://www.facebook.com/luke.barrett.169">Facebook</a><a href="https://github.com/brickmaker17">Github</a></footer>
         </div>
     )
 }
