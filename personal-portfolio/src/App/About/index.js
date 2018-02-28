08f1f5476a923c2d34451d273a84077be0bfceb3
Luke Barrett <gotluke@gmail.com>
2018-02-28 13:19:21 -0700

    Update to buisness time

--- personal-portfolio/src/App/About/index.js
+++ personal-portfolio/src/App/About/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 // import Nav from '../Nav/index';
 
 function About(props){
     return(
         <div>
             <h1>About Me</h1>
+            <p>test</p>
             <img src="" alt="Pic of me"/>
         </div>
     )
--- personal-portfolio/src/App/Contact/index.js
+++ personal-portfolio/src/App/Contact/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 // import Nav from '../Nav/index';
 
 function Contact(props){
